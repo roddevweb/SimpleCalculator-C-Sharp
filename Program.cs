6b8c9c1dda6c53ccb51306b922580cb1d1f183d4
rcoelhoweb <rcoelhoweb@gmail.com>
2024-04-22 17:13:39 -0400

    Simple Calculator in C#

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entrez le premiere nombre: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entrez le deuxième nombre");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int addition = number1 + number2;
+            int soustraction = number1 - number2;
+            int multiplication = number1 * number2;
+            int division = number1 / number2;
+
+            Console.WriteLine("Addition: " + addition);
+            Console.WriteLine("Soustraction: " + soustraction);
+            Console.WriteLine("Multiplication: " + multiplication);
+            Console.WriteLine("Division: " + division);
+
+            Console.ReadLine();
+
+        }
+    }
+}
